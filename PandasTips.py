16cec2e3e45eb7e387043091a3f85db842310b70
namnamu97 <37229892+namnamu97@users.noreply.github.com>
2020-10-30 06:39:19 +0000

    Update PandasTips.py

--- PandasTips.py
+++ PandasTips.py
@@ -163,3 +163,46 @@ def find_best_model_using_gridsearchcv(X,y):
     return pd.DataFrame(scores,columns=['model','best_score','best_params'])
 
 find_best_model_using_gridsearchcv(X,y)
+
+
+
+##############################################################
+#Pandas equivalent statement to SQL LIKE
+##############################################################
+
+#To find all the values from the series that starts with a pattern "s":
+SQL - WHERE column_name LIKE 's%'
+Python - column_name.str.startswith('s')
+
+#To find all the values from the series that ends with a pattern "s":
+SQL - WHERE column_name LIKE '%s'
+Python - column_name.str.endswith('s')
+
+#To find all the values from the series that contains pattern "s":
+SQL - WHERE column_name LIKE '%s%'
+Python - column_name.str.contains('s')
+
+
+
+##############################################################
+#Multiple Dataframes into Excel
+##############################################################
+
+import pandas as pd
+
+
+# Create some Pandas dataframes from some data.
+df1 = pd.DataFrame({'Data': [11, 12, 13, 14]})
+df2 = pd.DataFrame({'Data': [21, 22, 23, 24]})
+df3 = pd.DataFrame({'Data': [31, 32, 33, 34]})
+
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter('pandas_multiple.xlsx', engine='xlsxwriter')
+
+# Write each dataframe to a different worksheet.
+df1.to_excel(writer, sheet_name='Sheet1')
+df2.to_excel(writer, sheet_name='Sheet2')
+df3.to_excel(writer, sheet_name='Sheet3')
+
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()
