5604c3af65d428648ad8308d5cf943e819240267
namnamu97 <37229892+namnamu97@users.noreply.github.com>
2022-03-07 16:16:24 +0700

    update on argparse sample code

--- python_snippets.py
+++ python_snippets.py
@@ -11,6 +11,32 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 #######################################
+# argparse
+#######################################
+
+import argparse
+import math
+
+parser = argparse.ArgumentParser(description = 'Sample code for argparse')
+ 
+parser.add_argument('-r', '--radius', type = int, metavar = '', required = True, help = 'Radius of Cylinder')
+parser.add_argument('-H', '--height', type = int, metavar = '', required = True, help = 'Height of Cylinder')
+args = parser.parse_args()
+
+def cylinder_volume(radius, height):
+    vol = math.pi* (radius**2) * (height)
+    return vol
+
+if __name__ == '__main__':
+    print(cylinder_volume(args.radius, args.height))
+
+# eg the file name is test_argparse.py
+
+$ python3 test_argparse.py -r 3 -H 4
+
+
+
+#######################################
 # create virtualenv for python 3
 #######################################
 $ python3 -m venv python3_venv
